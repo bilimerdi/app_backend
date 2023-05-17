aaec30a18f1bb5f84317a9165080da5814858aa6
bilimerdi <orunerdi186@gmail.com>
2023-05-17 21:01:58 +0300

    Update Project
    veri tabanı oluşturuldu ve sütunlar belirlendi

--- app/src/main/java/com/example/app/entity/Hastalar.java
+++ app/src/main/java/com/example/app/entity/Hastalar.java
@@ -1,4 +1,30 @@
 package com.example.app.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.awt.*;
+import java.sql.Time;
+
+@Entity
 public class Hastalar {
+    @Id
+    @Column(name = "Dosya NO ")
+    private Long id;
+    @Column(name = "Hasta Ad Soyad")
+    private String fullName;
+    @Column(name="TC")
+    private String TC;
+    @Column(name = "Tanı Başlığı")
+    private String tani;
+    @Column(name = "Tanı Detayı")
+    private String taniDetay;
+    @Column(name = "Oluşturulan Tarih")
+    private byte[] Image ;
+    @Column(name = "Resim")
+    private Time time;
+    @Column(name = "Laborant Ad Soyad")
+    private String labFullname;
+
 }
